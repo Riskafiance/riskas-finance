fb6345049966eb67b749fb07cb9aa4ac0d79713c
Amarildi Riska <amarildiriska15@gmail.com>
2025-12-10 18:43:57 -0600

    Remove duplicate app from settings

--- riska_finance/settings.py
+++ riska_finance/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'accounting',  # <-- ADD YOUR APP HERE
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',  # This adds the commas to numbers
+    'accounting',
     ]
 
 MIDDLEWARE = [
